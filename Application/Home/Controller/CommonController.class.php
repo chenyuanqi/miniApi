359b5a9b9dd2f0258573b233ed7c4b6eefd9ccf0
chenyuanqi <chenyuanqi90s@163.com>
2015-08-29 15:42:09 +0800

    去除多余部分
    多余的注释

--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -12,14 +12,8 @@ class CommonController extends Controller {
      */
     public function _initialize() {
         // 设置报错级别
-        // error_reporting(0);
-/*$str = '{"errcode": "00002","errinfo": "注册失败"}';
-$foo = base64_encrypt($str); 
-$done= base64_decrypt(array('test' => $foo));
-O('加密前：'.$str);
-O('加密后：'.$foo);
-O('数据解密后：');
-O($done);*/
+        error_reporting(0);
+
     	// 左侧大分类数据
         $cateData = M('category')->where(array(
         	'status' => 1
